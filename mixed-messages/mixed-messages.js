9cf5e8f1ab8aa3fc500ce66cc9de971771658654
Mike <77694074+mikejm15@users.noreply.github.com>
2021-12-03 12:59:18 -0500

    adding first trial code of mixed messages
    adding my first idea for storing the message components for the
    project. Current idea is both unnecessarily and overly complicated.

--- mixed-messages/mixed-messages.js
+++ mixed-messages/mixed-messages.js
@@ -1,1 +1,7 @@
-
+// Arrays to store message components
+const subject = ['You', 'Life'];
+const linkingVerb = ['is', , 'should', 'may', 'could', 'have', 'shall', 'will', 'must'];
+const youVerbs = ['are', 'must', 'do', 'have', 'should', 'will', 'can'];
+const actionVerbs = ['live', 'run', 'get']
+const prepositions = ['in', ]
+const noun = ['life'];
